857ab275e3f7e83faa0200353aa30b25aa440fed
ldolieslager427-cmyk <ldolieslager427@gmail.com>
2025-12-24 13:32:01 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+{
+  id: "su-360",
+  name: "360 Spin",
+  level: "Intermediate",
+  how: "Pump hard, rotate shoulders first, keep knees bent.",
+  pre: ["180 slide", "Pumping"],
+  video: "https://www.youtube.com/embed/VIDEO_ID"
+}
